05b4d7a8a859eb072a90bcccd94201e8d0b7e298
Hendi Santika <hendisantika@gmail.com>
2021-10-15 08:43:29 +0700

    #9 - Add save & allByPostId in CommentRepository

--- src/main/java/com/hendisantika/repository/CommentRepository.java
+++ src/main/java/com/hendisantika/repository/CommentRepository.java
@@ -2,8 +2,12 @@ package com.hendisantika.repository;
 
 import com.hendisantika.domain.Comment;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.hibernate.orm.panache.runtime.JpaOperations;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +20,19 @@ import javax.enterprise.context.ApplicationScoped;
  */
 @ApplicationScoped
 public class CommentRepository implements PanacheRepositoryBase<Comment, String> {
+    @Transactional
+    public Comment save(Comment comment) {
+        EntityManager em = JpaOperations.INSTANCE.getEntityManager();
+        if (comment.getId() == null || comment.getId().trim().isEmpty()) {
+            em.persist(comment);
+            return comment;
+        } else {
+            return em.merge(comment);
+        }
+    }
+
+
+    public List<Comment> allByPostId(String id) {
+        return this.list("post.id", id);
+    }
 }
