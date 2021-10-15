95808dc039a2bf36399d5d2ee3b6455baa87d8e4
Hendi Santika <hendisantika@gmail.com>
2021-10-15 08:44:10 +0700

    #11 - Add findAllPosts in PostRepository

--- src/main/java/com/hendisantika/repository/PostRepository.java
+++ src/main/java/com/hendisantika/repository/PostRepository.java
@@ -2,8 +2,10 @@ package com.hendisantika.repository;
 
 import com.hendisantika.domain.Post;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.panache.common.Sort;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,5 +18,20 @@ import javax.enterprise.context.ApplicationScoped;
  */
 @ApplicationScoped
 public class PostRepository implements PanacheRepositoryBase<Post, String> {
+    public List<Post> findAllPosts() {
+        return this.listAll(Sort.descending("createdAt"));
+    }
 
+//    public Stream<Post> findByKeyword(String q, int offset, int limit) {
+//        if (q == null || q.trim().isEmpty()) {
+//            return this.streamAll(Sort.descending("createdAt"))
+//                    .skip(offset)
+//                    .limit(limit);
+//        } else {
+//            return this.streamAll(Sort.descending("createdAt"))
+//                    .filter(p -> p.title.contains(q) || p.content.contains(q))
+//                    .skip(offset)
+//                    .limit(limit);
+//        }
+//    }
 }
